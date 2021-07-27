01ffebf3a2101cc14a1343dbfa821b030feed489
youyi <46435808+youyi939@users.noreply.github.com>
2021-07-27 15:52:44 +0800

    抽离出自定义注解

---
+++ core/src/main/java/cn/lnfvc/ken/CoreMain.java
@@ -0,0 +1,12 @@
+package cn.lnfvc.ken;
+
+/**
+ * @Author: KenChen
+ * @Description:
+ * @Date: Create in  2021/7/27 下午3:49
+ */
+public class CoreMain {
+    public static void main(String[] args) {
+
+    }
+}
