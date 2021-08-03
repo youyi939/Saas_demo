a405a0e8ca727d7b5470a32bab384529f9e56c89
youyi <46435808+youyi939@users.noreply.github.com>
2021-08-03 17:01:24 +0800

    添加了自定义启动banner🧐 -彩虹猫

---
+++ Payment9092/src/test/java/UserServiceTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author: KenChen
+ * @Description:
+ * @Date: Create in  2021/8/3 下午3:07
+ */
+@SpringBootTest
+public class UserServiceTest {
+
+
+    @Test
+    public void test1(){
+        System.out.println("hello word");
+    }
+
+}
